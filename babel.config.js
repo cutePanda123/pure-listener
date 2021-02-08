ccafb280407d2d3d88ee8b8472ea373261e0c782
Peter Li <peterli1234@126.com>
2021-02-08 08:03:12 -0800

    use babel-plugin-module-resolver to manage paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@/utils': './src/utils',
+          '@/navigator': './src/navigator',
+          '@/pages': './src/pages',
+          '@/models': './src/models',
+          '@/config': './src/config',
+          '@/assets': './src/assets',
+          '@/components': './src/components'
+        }
+      }
+    ]
+  ]
 };
