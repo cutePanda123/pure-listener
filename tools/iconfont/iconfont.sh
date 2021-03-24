764421c27671583eb133aaab3c25d851d68b64ae
Peter Li <peterli1234@126.com>
2021-03-23 21:34:06 -0700

    updated iconfont setup.sh

--- tools/iconfont/iconfont.sh
+++ tools/iconfont/iconfont.sh
@@ -1,12 +1,13 @@
 # step 1
 npm install --save-dev react-native-iconfont-cli
-# step 2
+
+# step 2: init iconfont in the project
 npx iconfont-init
+
 # step 3
 # go to https://www.iconfont.cn/ and add icons into a project
 # generate the symbol links in iconfont webpage
 # copy paste the link to iconfont.json
-# command to generate iconfont svgs and save them in the repo
 
-# step 4
+# step 4: generate iconfont svgs and save them in the repo
 npx iconfont-rn
