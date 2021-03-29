cf3fde781b463ab98901a74250094b3d7962b3f7
Peter Li <peterli1234@126.com>
2021-03-29 14:11:30 -0700

    added linear gradient color changes on the home tab navigation

--- src/navigator/HomeTabs.tsx
+++ src/navigator/HomeTabs.tsx
@@ -1,44 +1,56 @@
 import React from 'react';
-import { createMaterialTopTabNavigator, MaterialTopTabBar, MaterialTopTabBarProps } from '@react-navigation/material-top-tabs';
+import {
+  createMaterialTopTabNavigator,
+  MaterialTopTabBar,
+  MaterialTopTabBarProps,
+} from '@react-navigation/material-top-tabs';
 import Home from '@/pages/Home';
-import { View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import TopTabBarWrapper from '@/pages/Views/TopTabBarWrapper';
 
 const Tab = createMaterialTopTabNavigator();
 
 class HomeTabs extends React.Component {
-    renderTabBar = (props: MaterialTopTabBarProps) => {
-        return (
-            <TopTabBarWrapper {...props}/>
-        );
-    }
-    render() {
-        return (
-            <Tab.Navigator
-                lazy={true}
-                tabBar={this.renderTabBar}
-                tabBarOptions={{
-                    scrollEnabled: true,
-                    tabStyle: {
-                        width: 150,
-                    },
-                    indicatorStyle: {
-                        height: 4,
-                        width: 20,
-                        marginLeft: 30,
-                        borderRadius: 2,
-                        backgroundColor: '#f86442',
-                    },
-                    activeTintColor: '#f86442',
-                    inactiveTintColor: '#333',
-                }}
-            >
-                <Tab.Screen name='Home' component={Home} options ={{ 
-                    tabBarLabel: 'Recommendation',
-                }}/>
-            </Tab.Navigator>
-        )
-    }
+  renderTabBar = (props: MaterialTopTabBarProps) => {
+    return <TopTabBarWrapper {...props} />;
+  };
+  render() {
+    return (
+      <Tab.Navigator
+        lazy={true}
+        tabBar={this.renderTabBar}
+        sceneContainerStyle={styles.sceneContainer}
+        tabBarOptions={{
+          scrollEnabled: true,
+          tabStyle: {
+            width: 150,
+          },
+          indicatorStyle: {
+            height: 4,
+            width: 20,
+            marginLeft: 30,
+            borderRadius: 2,
+            backgroundColor: '#f86442',
+          },
+          activeTintColor: '#f86442',
+          inactiveTintColor: '#333',
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Recommendation',
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+    sceneContainer: {
+        backgroundColor: 'transparent',
+    },
+});
 
 export default HomeTabs;
--- src/pages/Views/TopTabBarWrapper.tsx
+++ src/pages/Views/TopTabBarWrapper.tsx
@@ -1,29 +1,84 @@
-import { MaterialTopTabBar, MaterialTopTabBarProps } from '@react-navigation/material-top-tabs';
+import Touchable from '@/components/Touchable';
+import {
+  MaterialTopTabBar,
+  MaterialTopTabBarProps,
+} from '@react-navigation/material-top-tabs';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {getStatusBarHeight} from 'react-native-iphone-x-helper';
-
-interface IProps extends MaterialTopTabBarProps {
-
-}
+import LinearGradient from 'react-native-linear-gradient';
+ 
+interface IProps extends MaterialTopTabBarProps {}
 
 class TopTabBarWrapper extends React.Component<IProps> {
-    render() {
-        const {props} = this;
-        return (
-            <View style={styles.container}>
-                <MaterialTopTabBar
-                    {...props}
-                />
-            </View>
-        );
-    }
+  render() {
+    const {props} = this;
+    return (
+      <View style={styles.container}>
+          <LinearGradient
+            colors={['#ccc', '#e2e2e2']}
+            style={styles.gradient}
+          />
+        <View style={styles.topTabBarViewTop}>
+          <MaterialTopTabBar {...props} style={styles.tabBar} />
+          <Touchable style={styles.topTabBarCategory}>
+            <Text>Category</Text>
+          </Touchable>
+        </View>
+        <View style={styles.topTabBarViewBottom}>
+          <Touchable style={styles.topTabBarSearch}>
+            <Text>Search</Text>
+          </Touchable>
+          <Touchable style={styles.topTabBarHistory}>
+            <Text>History</Text>
+          </Touchable>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: getStatusBarHeight(),
-    },
+  container: {
+    backgroundColor: '#fff',
+    paddingTop: getStatusBarHeight(),
+  },
+  tabBar: {
+    elevation: 0,
+    flex: 1,
+    overflow: 'hidden',
+    backgroundColor: 'transparent',
+  },
+  topTabBarViewTop: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  topTabBarCategory: {
+    paddingHorizontal: 10,
+    borderLeftWidth: StyleSheet.hairlineWidth,
+    borderLeftColor: '#ccc',
+  },
+  topTabBarViewBottom: {
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    paddingVertical: 7,
+    alignItems: 'center',
+  },
+  topTabBarSearch: {
+    flex: 1,
+    paddingLeft: 12,
+    height: 30,
+    justifyContent: 'center',
+    borderRadius: 15,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  topTabBarHistory: {
+    marginLeft: 24,
+  },
+  gradient: {
+      ...StyleSheet.absoluteFillObject,
+      height: 260,
+  }
 });
 
 export default TopTabBarWrapper;
