8aa3e9b420de8b7c77ed1db88a0767ed7622ff3f
Peter Li <peterli1234@126.com>
2021-04-04 20:43:09 -0700

    implemented category dva redux state

--- src/models/category.ts
+++ src/models/category.ts
@@ -1,39 +1,95 @@
-import { Model, Effect, SubscriptionsMapObject } from 'dva-core-ts';
-import { Reducer } from 'redux';
+import storage, {load} from '@/config/storage';
+import axios from 'axios';
+import {Model, Effect, SubscriptionsMapObject} from 'dva-core-ts';
+import {Reducer} from 'redux';
+
+const CATEGORY_DATA_URL = '/mock/29/category';
 
 interface ICategory {
-    id: string;
-    name: string;
-    classify?: string;
-};
+  id: string;
+  name: string;
+  classify?: string;
+}
 
-interface CategoryModelState {
-    selectedCategories: ICategory[];
-    candidateCategories: ICategory[];
-};
+export interface CategoryModelState {
+  selectedCategories: ICategory[];
+  candidateCategories: ICategory[];
+}
 
 interface CategoryModel extends Model {
-    namespace: 'category',
-    state: CategoryModelState;
-    effects: {
-        loadData: Effect;
-    },
-    reducers: {
-        setState: Reducer<CategoryModelState>;
-    },
-    subscriptions: SubscriptionsMapObject;
-};
+  namespace: 'category';
+  state: CategoryModelState;
+  effects: {
+    loadData: Effect;
+  };
+  reducers: {
+    setState: Reducer<CategoryModelState>;
+  };
+  subscriptions: SubscriptionsMapObject;
+}
 
 const initialState: CategoryModelState = {
-    selectedCategories: [
-        {
-            id: 'home',
-            name: 'Recommend',
-        },
-        {
-            id :'vip',
-            name: 'VIP',
-        },
-    ],
-    candidateCategories: [],
+  selectedCategories: [
+    {
+      id: 'home',
+      name: 'Recommend',
+    },
+    {
+      id: 'vip',
+      name: 'VIP',
+    },
+  ],
+  candidateCategories: [],
 };
+
+const categoryModel: CategoryModel = {
+  namespace: 'category',
+  state: initialState,
+  effects: {
+    *loadData(_, {call, put}) {
+      const selectedCategories = yield call(load, {key: 'selectedCategories'});
+      const candidateCategories = yield call(load, {key: 'selectedCategories'});
+      if (selectedCategories) {
+        yield put({
+          type: 'setState',
+          payload: {
+            selectedCategories,
+            candidateCategories,
+          },
+        });
+      } else {
+        yield put({
+          type: 'setState',
+          payload: {
+            candidateCategories,
+          },
+        });
+      }
+    },
+  },
+  reducers: {
+    setState(state, {payload}) {
+      return {
+        ...state,
+        ...payload,
+      };
+    },
+  },
+  // dva will call all the functions inside subscriptions after startup
+  subscriptions: {
+    setup({dispatch}) {
+      dispatch({type: 'loadData'});
+    },
+    asyncStorage() {
+      storage.sync.candidateCategories = async () => {
+        const {data} = await axios.get(CATEGORY_DATA_URL);
+        return data;
+      };
+      storage.sync.selectedCategories = async () => {
+        return null;
+      };
+    },
+  },
+};
+
+export default categoryModel;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,12 @@
 import home, {HomeState} from './home';
 import { DvaLoadingState } from 'dva-loading-ts';
+import category, {CategoryModelState} from './category';
 
-const models = [home];
+const models = [home, category];
 
 export type RootState = {
     home: HomeState;
+    category: CategoryModelState;
     loading: DvaLoadingState;
 }
 
