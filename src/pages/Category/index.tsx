cce86b0b19d5484460139ae59f58b054f7a02f39
Peter Li <peterli1234@126.com>
2021-04-05 22:37:52 -0700

    group category in category views

--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {RootState} from '@/models/';
+import {RootState} from '@/models/index';
 import {connect, ConnectedProps} from 'react-redux';
 import {ICategory} from '@/models/category';
-import { viewPortWidth } from '@/utils/';
+import _ from 'lodash';
+import {ScrollView} from 'react-native-gesture-handler';
+import CategoryItem from './CategoryItem';
 
 const mapStateToProps = ({category}: RootState) => {
   return {
@@ -20,9 +22,6 @@ interface IState {
   selectedCategories: ICategory[];
 }
 
-const containerDivWidth = viewPortWidth - 10;
-const categoryItemWidth = containerDivWidth / 4;
-
 class Category extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
@@ -33,52 +32,55 @@ class Category extends React.Component<IProps, IState> {
   render() {
     const {candidateCategories} = this.props;
     const {selectedCategories} = this.state;
+    const categoryGroup = _.groupBy(
+      candidateCategories,
+      (category) => category.category,
+    );
+
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <Text style={styles.title}>My categories</Text>
         <View style={styles.items}>
-            {selectedCategories.map(this.renderCategoryItem)}
+          {selectedCategories.map(this.renderCategoryItem)}
         </View>
         <Text style={styles.title}>All categories</Text>
         <View style={styles.items}>
-            {candidateCategories.map(this.renderCategoryItem)}
+          {Object.keys(categoryGroup).map((category) => {
+            return (
+              <View key={category}>
+                <Text style={styles.title}>{category}</Text>
+                <View>
+                  {categoryGroup[category].map(this.renderCategoryItem)}
+                </View>
+              </View>
+            );
+          })}
         </View>
-      </View>
+      </ScrollView>
     );
   }
 
   renderCategoryItem = (category: ICategory, idx: number) => {
-      return (
-          <View key={category.id} style={{
-              width: categoryItemWidth,
-              backgroundColor: '#fff',
-              height: 40,
-              justifyContent: 'center',
-              alignItems: 'center',
-              padding: 5,
-              flex: 1,
-          }}>
-            <Text>{category.name}</Text>
-          </View>
-      );
-  }
+    return <CategoryItem data={category} />;
+  };
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f3f6f6',
-    },
-    title: {
-        fontSize: 16,
-        marginTop: 14,
-        marginBottom: 8,
-    },
-    items: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        padding: 5,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#f3f6f6',
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 14,
+    marginBottom: 8,
+    marginLeft: 10,
+  },
+  items: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 5,
+  },
 });
 
 export default connector(Category);
