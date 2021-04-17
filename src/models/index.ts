5cbefa0aa05e75b4478c6bba5732d10023afe3ba
cutePanda123 <peterli1234@126.com>
2021-04-17 14:07:20 -0700

    added channel detail to root state

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,15 +1,15 @@
 import home, {HomeState} from './home';
 import { DvaLoadingState } from 'dva-loading-ts';
 import category, {CategoryModelState} from './category';
-import channel from './channel';
+import channelDetail from './channelDetail';
 
-const models = [home, category, channel];
+const models = [home, category, channelDetail];
 
 export type RootState = {
     home: HomeState;
     category: CategoryModelState;
     loading: DvaLoadingState;
-    channel: typeof channel.state;
+    channelDetail: typeof channelDetail.state;
 } & {
     [key: string]: typeof home.state;
 };
